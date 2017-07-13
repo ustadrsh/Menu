caf9474ba4366f9757abba96bffb97eb1f450ea3
Henry Chan <henrychan699966@gmail.com>
2017-07-13 22:20:06 +0800

    content provider of File tested

--- app/src/main/java/hk/ust/aed/menu/UploadFirebase.java
+++ app/src/main/java/hk/ust/aed/menu/UploadFirebase.java
@@ -7,6 +7,15 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 /**
  * Created by henry on 7/4/17.
@@ -23,22 +32,28 @@ public class UploadFirebase extends AsyncTask<Void, Void, Void> {
     @Override
     protected Void doInBackground(Void... params) {
         Log.e("UploadFirebase","doInBackground");
-        ContentResolver contentResolver = context.getContentResolver();
-        Cursor cursor = contentResolver.query(Uri.parse(context.getResources().getString(R.string.PM_URI_PhoneUsage)),null,null,null,null);
+        DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
 
-        if(cursor == null){
-            Log.e("CONTENT RESOLVER","null cursor");
-            return null;
+        Uri uri = Uri.parse(context.getResources().getString(R.string.PM_URI) + dateFormat.format(new Date(System.currentTimeMillis())));
+        InputStream is = null;
+        StringBuilder result = new StringBuilder();
+        try {
+            is = context.getContentResolver().openInputStream(uri);
+            BufferedReader r = new BufferedReader(new InputStreamReader(is));
+            String line;
+            while ((line = r.readLine()) != null) {
+                result.append(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try { if (is != null) is.close(); } catch (IOException e) { }
         }
 
-        if(cursor.getCount() < 1){
-            Log.e("CONTENT RESOLVER","empty cursor");
-            return null;
-        }
+        Log.e("TestJson",result.toString());
 
-        cursor.moveToFirst();
-
-        Log.e("CONTENT RESOLVER",cursor.getString(1));
         return null;
     }
 }

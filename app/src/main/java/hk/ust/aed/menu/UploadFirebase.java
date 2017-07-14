2fb54c7bbbe3bce2314ee3f9cd0e57fb78a88eb1
Henry Chan <henrychan699966@gmail.com>
2017-07-14 12:48:11 +0800

    upload firebase methods finished and tested

--- app/src/main/java/hk/ust/aed/menu/UploadFirebase.java
+++ app/src/main/java/hk/ust/aed/menu/UploadFirebase.java
@@ -8,13 +8,22 @@ import android.os.AsyncTask;
 import android.util.Log;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 
 /**
@@ -23,18 +32,38 @@ import java.util.Date;
 
 public class UploadFirebase extends AsyncTask<Void, Void, Void> {
     private Context context;
+    private String filesDir;
+    private String directoryPM;
+    private static final String FIREBASE_URL = "https://ustadrsh-cf116.firebaseio.com/users/";
 
     public UploadFirebase(Context context) {
         this.context = context;
+
+        filesDir = context.getFilesDir().toString();
+
+        File dirPM = new File(filesDir,"/PassiveMon");
+        if(!dirPM.exists()) dirPM.mkdir();
+        directoryPM = dirPM.toString();
+        Log.i("PMdatadirectory",directoryPM);
     }
 
 
     @Override
     protected Void doInBackground(Void... params) {
         Log.e("UploadFirebase","doInBackground");
-        DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
 
-        Uri uri = Uri.parse(context.getResources().getString(R.string.PM_URI) + dateFormat.format(new Date(System.currentTimeMillis())));
+        String[] dataPM = passiveMonGetData(context);
+        dataUploadPM(dataPM[0],dataPM[1],"user123456");
+
+
+
+        return null;
+    }
+
+    private String[] passiveMonGetData(Context context){
+        DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+        String trialID = dateFormat.format(new Date(System.currentTimeMillis()));
+        Uri uri = Uri.parse(context.getResources().getString(R.string.PM_URI) + trialID);
         InputStream is = null;
         StringBuilder result = new StringBuilder();
         try {
@@ -53,7 +82,46 @@ public class UploadFirebase extends AsyncTask<Void, Void, Void> {
         }
 
         Log.e("TestJson",result.toString());
+        String[] data = {result.toString(),trialID};
+        return data;
+    }
 
-        return null;
+    //true if file store successfully
+    private boolean dataStorePM(String data, String fileName){
+        FileOutputStream outputStream;
+
+        try {
+            outputStream = context.openFileOutput(fileName, Context.MODE_PRIVATE);
+            outputStream.write(data.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        File file = new File(directoryPM, fileName + ".json");
+
+        return file.length()> 1;
+    }
+
+    private boolean dataUploadPM(String data, String trialID, String userID){
+        OkHttpClient client = new OkHttpClient();
+        final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+        RequestBody body = RequestBody.create(JSON, data);
+        Response response = null;
+        Request request = new Request.Builder()
+                .url(FIREBASE_URL + userID + "/passiveMon/" + trialID + ".json")
+                .post(body)
+                .build();
+        try{
+            response = client.newCall(request).execute();
+        }
+        catch (IOException e){
+
+        }
+
+        if(response == null) return false;
+        if(!response.isSuccessful()) return false;
+        return true;
     }
 }
